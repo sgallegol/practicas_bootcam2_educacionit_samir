166fb86d276c5d2451f3b6adf947e785c17ddf79
sgallegol <sgallegol@unal.edu.co>
2022-10-07 15:42:22 -0500

    clase 10
    -repaso oop

--- app-web/src/main/java/clase10/Mamiferos.java
+++ app-web/src/main/java/clase10/Mamiferos.java
@@ -1,5 +1,19 @@
 package clase10;
+//tiene lo mismo que un animal, en cuanto a atributos
+//nacen de una amnera particular
+//sus propias métodos
 
-public class Mamiferos {
+public class Mamiferos extends Animal {
+	
+	private Integer tiempoGestacion;
+
+	public Mamiferos(String color, Integer tiempoGestacion) {
+		super(color);
+		this.tiempoGestacion = tiempoGestacion;
+		
+		super.getEdad();
+	}
+
+	
 
 }
--- app-web/src/main/java/clase10/Oviparos.java
+++ app-web/src/main/java/clase10/Oviparos.java
@@ -1,5 +1,11 @@
 package clase10;
 
 public class Oviparos {
-
+	//tiene lo mismo que un animal, en cuanto a atributos
+		//nacen de una amnera particular
+		//sus propias métodos
+	
+	
+	
+	
 }
